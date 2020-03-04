e20f1d142ebef19f9e32f914bfed66d9aea60dde
Uttakarsh Tikku <uttakarsh.tikku.1993@gmail.com>
2020-03-05 01:15:19 +1100

    Added contraction support

--- tools/expand_contractions.py
+++ tools/expand_contractions.py
@@ -1,12 +1,36 @@
+import os
+import json
 
 def run(payload):
+    contractions = {}
+    with open('{}/resources/contractions.json'.format(os.getcwd()), 'r') as f:
+        contractions = json.load(f)
     
+    contractions = dict((k.lower(), v) for k,v in contractions.items())
+
     text = payload['text']
 
     if isinstance(text, str):
-        text = text.lower()
+        temp_text = []
+        
+        for word in text.split():
+            if word.lower() in contractions.keys():
+                temp_text.append(contractions[word.lower()])
+            else:
+                temp_text.append(word)
+
+        text = ' '.join(temp_text)
+
     else:
-        text = [word.lower() for word in text]
+        temp_text = []
+        
+        for word in text:
+            if word.lower() in contractions.keys():
+                temp_text.append(contractions[word.lower()])
+            else:
+                temp_text.append(word)
+
+        text = temp_text
 
     payload['text'] = text
 
